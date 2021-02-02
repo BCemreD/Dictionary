298e5b56f5490b9fb8e6ead811126144bae268e4
BCemreD <72667928+BCemreD@users.noreply.github.com>
2021-02-02 21:24:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> employeId = new Dictionary<int, string>();
+            employeId.Add(155, "Canan");
+            employeId.Add(187, "Bülent");
+            employeId.Add(166, "Ayşe");
+            employeId.Add(145, "Mesut");
+
+            if (employeId.ContainsKey(187))
+            {
+                string value = employeId[187];
+                Console.WriteLine("Aradığınız Çalışan" + " " + value);
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
